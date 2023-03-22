cf07c2bb8d8d8d0523e5fe91d9d58aa430461959
araya <araya@araya.dev>
2023-03-23 00:26:41 +0900

    Use ESM

--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
-require('dotenv').config()
+import dotenv from 'dotenv';
+import server from './lib/server.mjs'
+dotenv.config()
 
-const app = require('./lib/server')()
+
+const app = server()
 const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log('Listening at http://localhost:' + port)
